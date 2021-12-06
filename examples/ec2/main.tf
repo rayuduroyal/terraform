e5967b4b140877c66fb3af280280c18c28ccdf76
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-06 14:52:12 +0530

    update terraform

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -10,6 +10,9 @@ resource "aws_instance" "sample" {
 }
 
 resource "null_resource" "sample" {
+  triggers = {
+    abc = timestamp()
+  }
   provisioner "remote-exec" {
     connection {
       host     = aws_instance.sample.*.public_ip[0]
