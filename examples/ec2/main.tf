657097bd2a175582f7c61c247108c575a741bceb
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-06 12:52:44 +0530

    auto pushing chainges repo

---
+++ examples/ec2/main.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "sample" {
+  count                  = var.env == "prod" ? 1 : 0
+  ami                    = data.aws_ami.example.id
+  instance_type          = var.instance_type == "" ? "t3.micro" : var.instance_type
+  vpc_security_group_ids = [var.SGID]
+
+  tags = {
+    Name = element(var.name, count.index)
+  }
+}
+
+variable "SGID" {}
+variable "name" {}
+
+variable "instance_type" {}
+variable "env" {}
+
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "^Ubuntu*"
+  owners      = ["973714476881"]
+}
+
---
+++ examples/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform.b60"
+    key    = "example/sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+module "ec2" {
+  source        = "./ec2"
+  SGID          = module.sg.SGID
+  name          = ["new1", "new2"]
+  instance_type = var.instance_type
+  env           = var.env
+}
+
+module "sg" {
+  source = "./sg"
+}
+
+variable "instance_type" {}
+variable "env" {}
+
+
