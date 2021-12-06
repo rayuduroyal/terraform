a0550cdf3fe1fdf7cba2979f00c05c19e5c4226b
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-06 15:27:57 +0530

    update terraform

---
+++ roboshop/main.tf
@@ -0,0 +1,20 @@
+resource "aws_spot_instance_request" "cheap_worker" {
+  count                  = length(var.components)
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-000e3d77edcfc8e1f"]
+
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "^Ubuntu*"
+  owners      = ["973714476881"]
+}
+
+variable "components" {
+  default = ["frontend", "mongodb", "catalogue", "cart", "user", "redis", "mysql", "shipping", "rabbitmq", "payment"]
+}
