ffd9c171e6ed1c04bcb964a204e8f03e33f742ca
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-07 11:42:40 +0530

    update terraform

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,12 +1,19 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count                  = length(var.components)
   ami                    = data.aws_ami.ami.id
-  instance_type          = "t3-micro"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-000e3d77edcfc8e1f"]
+  wait_for_fulfillment   = true
 
   tags = {
     Name = element(var.components, count.index)
   }
+}
+resource "aws_ec2_tag" "tags" {
+  count       = length(var.components)
+  resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key         = "Name"
+  value       = element(var.components, count.index)
 }
 
 data "aws_ami" "ami" {
