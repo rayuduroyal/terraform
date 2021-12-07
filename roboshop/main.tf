7087fcddb3a0742129b33fcc333e411fb5e980b4
susarlarayudu <rayuduroyal1996@gmail.com>
2021-12-07 13:23:01 +0530

    update terraform

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -5,14 +5,32 @@ resource "aws_spot_instance_request" "cheap_worker" {
   vpc_security_group_ids = ["sg-000e3d77edcfc8e1f"]
   wait_for_fulfillment   = true
   tags = {
-    Name = element(var.components, count.index)
+    Name = local.COMP_NAME
   }
 }
 resource "aws_ec2_tag" "tags" {
   count       = length(var.components)
   resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key         = "Name"
-  value       = element(var.components, count.index)
+  value       = local.COMP_NAME
+}
+
+resource "null_resource" "ansible" {
+  count       = length(var.components)
+  provisioner "remote_exec" {
+    connection {
+      host     = element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)
+      user     = "centos"
+      password = "DevOps321"
+    }
+    inline = [
+      "yum install python3-pip -y",
+      "pip3 install pip --upgrade",
+      "pip3 install ansible",
+      "ansible-pull -u git@github.com:rayuduroyal/ansible.git roboshop-pull.yaml -e COMPONENT=${local.COMP_NAME}"
+
+    ]
+  }
 }
 
 data "aws_ami" "ami" {
@@ -28,4 +46,8 @@ variable "components" {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+locals {
+  COMP_NAME = element(var.components, count.index)
 }
